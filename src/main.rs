858b212fbf6d44ed1e0af0a151f16c9f4462e0ba
Alex Chan <alex@alexwlchan.net>
2022-03-13 10:47:26 +0000

    Render a new template

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tera::Tera;
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("templates/**/*") {
+        let mut tera = match Tera::new("new_templates/**/*") {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error(s): {}", e);
@@ -18,9 +18,7 @@ lazy_static! {
 }
 
 fn main() {
-    println!("Hello world");
-
     let context = tera::Context::new();
 
-    println!("{}", (*TEMPLATES).render("templates/base.html", &context).unwrap());
+    println!("{}", (*TEMPLATES).render("base.html", &context).unwrap());
 }
