9711912df1bef81eda79773170fb1f7ccd0b02c7
Alex Chan <alex@alexwlchan.net>
2022-03-13 10:51:02 +0000

    make the code more modular

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
 #[macro_use]
 extern crate lazy_static;
 
-use tera::Tera;
-
-lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("new_templates/**/*") {
-            Ok(t) => t,
-            Err(e) => {
-                println!("Parsing error(s): {}", e);
-                ::std::process::exit(1);
-            }
-        };
-        tera.autoescape_on(vec![".html"]);
-        tera
-    };
-}
+mod templates;
 
 fn main() {
     let context = tera::Context::new();
 
-    println!("{}", (*TEMPLATES).render("base.html", &context).unwrap());
+    println!("{:?}", templates::render("base.html", &context));
 }
---
+++ src/templates.rs
@@ -0,0 +1,19 @@
+use tera::{Context, Tera};
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("new_templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec![".html"]);
+        tera
+    };
+}
+
+pub fn render(name: &str, context: &Context) -> Result<std::string::String, tera::Error> {
+    (*TEMPLATES).render(name, context)
+}
