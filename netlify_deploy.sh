b0f17db53cb8d0220ad9f8f3e0561443f641f345
Alex Chan <alex@alexwlchan.net>
2022-05-28 16:16:25 +0100

    Revert "Try to make more tolerant Netlify builds post-new-release"
    20e870b08f9de5db8eaf81eb23f9d3a0456ae94e

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -4,25 +4,9 @@
 set -o errexit
 set -o nounset
 
-# When I publish a new version, GitHub Actions will create a new release
-# that Netlify will try to pull from -- but it will discover there aren't
-# any binaries yet.
-#
-# It should wait about ten minutes for binaries to become available -- long
-# enough that it should always succeed, but not so long it uses all my
-# Netlify build minutes waiting for GitHub Actions.
-for i in {1,..,600}
-do
-  DOWNLOAD_URL=$(curl --silent 'https://api.github.com/repos/alexwlchan/books.alexwlchan.net/releases/latest' \
-    | jq -r ' .assets | map(.browser_download_url) | map(select(test(".*linux.*")))[0]'
-  )
-  if [[ "$DOWNLOAD_URL" != "null" ]]
-  then
-    break
-  else
-    sleep 1
-  fi
-done
+DOWNLOAD_URL=$(curl --silent 'https://api.github.com/repos/alexwlchan/books.alexwlchan.net/releases/latest' \
+  | jq -r ' .assets | map(.browser_download_url) | map(select(test(".*linux.*")))[0]'
+)
 
 # The --location flag means we follow redirects
 curl --location "$DOWNLOAD_URL" > ~/.cargo/bin/vfd.tar.gz
