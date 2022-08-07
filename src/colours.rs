5ec5abccaa521a891ec00e095eda06dc66160029
Alex Chan <alex@alexwlchan.net>
2022-08-07 08:25:27 +0100

    colour, not tuple

--- src/colours.rs
+++ src/colours.rs
@@ -1,7 +1,7 @@
 use palette::Srgb;
 use std::str::FromStr;
 
-// Parses a hex string as an RGB tuple, e.g. #d01c11 ~> (208, 28, 17)
+// Parses a hex string as an RGB colour, e.g. #d01c11 ~> (208, 28, 17)
 //
 // This function assumes the hex string is correctly formatted.
 pub fn parse_hex_string(s: &str) -> Srgb<u8> {
