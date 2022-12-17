c3c63469cddc9a31ce3505d28bd8621159440b7d
Alex Chan <alex@alexwlchan.net>
2022-12-17 00:53:05 +0000

    WIP keep plugging away

--- static/filters.js
+++ static/filters.js
@@ -69,6 +69,18 @@ function getAuthorNames() {
    return authors;
 }
 
+function createEmptyFilters() {
+  return {
+    authors: [],
+    publicationYear: {
+      before: undefined,
+      after: undefined,
+    },
+    starRating: undefined,
+    tags: [],
+  };
+}
+
 /** Returns true if a given book matches the specified filters.
   *
   * Here the `filters` has the following structure:
@@ -86,7 +98,7 @@ function getAuthorNames() {
 function matchesFilters(book, filters) {
 
   // It's sufficient to match a single author in the list.
-  const authors = getAuthorNames(book.getAttribute('data-book-authors'));
+  const authors = parseAuthorNames(book.getAttribute('data-book-authors'));
 
   const matchesAuthorFilter =
     filters.authors.length === 0 ||
@@ -139,4 +151,136 @@ function Counter(array) {
   var count = {};
   array.forEach(val => count[val] = (count[val] || 0) + 1);
   return count;
+}
+
+/** Create the "Tippy" popovers for each filter.
+  *
+  * Because the Tippy content isn't accessible when the popover is closed,
+  * we have "createTippy()" functions that should be called every time the
+  * filter content changes outside the popover itself (i.e. in removeFilter)
+  */
+function createTippy(id, content) {
+  tippy(id, {
+    content,
+    trigger: 'click',
+    placement: 'bottom',
+    allowHTML: true,
+    theme: 'light',
+    // The default width is 350px, but we want arbitrary width
+    maxWidth: '',
+    // https://atomiks.github.io/tippyjs/v6/all-props/#interactive
+    interactive: true,
+  });
+}
+
+/** Create the popup box for author filters. */
+function createAuthorFilter(id, filters) {
+  const authors = getAuthorNames();
+
+  createTippy(
+    '#authorFilters',
+    `
+      <ul id="author_filters">
+        ${
+          authors.map((name, i) =>
+            `<li>
+              <input
+                id="author:${name}"
+                type="checkbox"
+                ${filters.authors.indexOf(name) !== -1 ? 'checked' : ''}
+                name="author"
+                data-author-name="${name}"
+                onchange="applyAuthorFilters('ul#author_filters input', filters)"
+              >
+              <label for="author:${name}">${name}</label>
+            </li>`
+          ).join("")
+        }
+      </ul>
+    `);
+}
+
+function applyAuthorFilters(selector, filters) {
+  const selectedAuthors = Array.from(document.querySelectorAll(selector))
+    .filter(input => input.checked)
+    .map(input => input.getAttribute("data-author-name"));
+
+  // This ensures we preserve the order in which filters were applied: any
+  // authors you'd already selected retain their position in the list, and
+  // new authors appear at the end.
+  const existingAuthors = filters['authors'].filter(a => selectedAuthors.indexOf(a) !== -1);
+  const newAuthors = selectedAuthors.filter(a => filters['authors'].indexOf(a) === -1);
+
+  filters['authors'] = [...existingAuthors, ...newAuthors];
+
+  applyFilters(filters);
+}
+
+/** Apply the current set of filters to the page.
+  *
+  * This updates the page state, including which reviews/headings should
+  * be visible.  Call this whenever the filter state changes.
+  */
+function applyFilters(filters) {
+  const hasFiltersApplied = JSON.stringify(filters) !== JSON.stringify(createEmptyFilters());
+
+  console.log(JSON.stringify(filters));
+
+  const selectedReviews = Array.from(document.querySelectorAll('.review_preview'))
+    .filter(rev => matchesFilters(rev, filters));
+
+  const selectedReviewIds = new Set(selectedReviews.map(rp => rp.getAttribute("id")));
+
+  // How many reviews did I write/finish in each year?  Note that these
+  // are slightly different, e.g. if I filter for books by an author and
+  // I didn't finish any of their books in a year
+  const yearReviewTally = Counter(
+    selectedReviews
+      .map(rev => rev.getAttribute('data-review-year'))
+  );
+
+  const yearFinishedTally = Counter(
+    selectedReviews
+      .filter(rev => !rev.hasAttribute('data-did-not-finish'))
+      .map(rev => rev.getAttribute('data-review-year'))
+  );
+
+  // Show/hide the individual reviews
+  document.querySelectorAll('.review_preview').forEach(rev => {
+    if (rev.getAttribute('data-review-year') === 'another time') {
+      rev.style.display = selectedReviewIds.has(rev.getAttribute('id')) ? 'grid' : 'none';
+    } else {
+      rev.style.display = selectedReviewIds.has(rev.getAttribute('id')) ? 'block' : 'none';
+    }
+  })
+
+  // Show/hide the year headings, and the dividers between them.
+  document.querySelectorAll('.year_heading').forEach(yh => {
+    const thisYear = yh.getAttribute('data-year');
+
+    yh.style.display = yearReviewTally[thisYear] > 0 ? 'block' : 'none';
+
+    const reviewCount = yearReviewTally[thisYear];
+    const finishedCount = yearFinishedTally;
+
+    const everythingIsFinished = reviewCount === finishedCount;
+    const isCurrentYear = yh.hasAttribute('data-is-current-year');
+
+    if (everythingIsFinished && isCurrentYear) {
+      yh.innerHTML = `the ${finishedCount} book${finishedCount > 1 ? 's' : ''} i’ve finished so far in ${thisYear}`;
+    } else if (thisYear === 'another time') {
+      yh.innerHTML = 'books i read at another time';
+    } else if (everythingIsFinished || finishedCount > 5) {
+      yh.innerHTML = `the ${finishedCount} book${finishedCount > 1 ? 's' : ''} i’ve finished so far in ${thisYear}`;
+    }
+
+    if (yearReviewTally[thisYear] === yearFinishedTally)
+
+  })
+
+  document.querySelectorAll('.divider').forEach(dv =>
+    dv.style.display = yearReviewTally[dv.getAttribute('data-year')] > 0 ? 'block' : 'none'
+  );
+
+  console.log(selectedReviews);
 }
