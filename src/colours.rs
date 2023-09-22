63e824ac5686fff126e938fbb744c0fec7de4787
Alex Chan <alex@alexwlchan.net>
2023-09-23 00:25:31 +0100

    keep pruning

--- src/colours.rs
+++
@@ -1,9 +0,0 @@
-use palette::Srgb;
-use std::str::FromStr;
-
-// Parses a hex string as an RGB colour, e.g. #d01c11 ~> (208, 28, 17)
-//
-// This function assumes the hex string is correctly formatted.
-pub fn parse_hex_string(s: &str) -> Srgb<u8> {
-    Srgb::from_str(s).unwrap()
-}
