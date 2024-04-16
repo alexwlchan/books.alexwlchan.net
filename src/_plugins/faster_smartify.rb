ddaaec54f3d5426a1318625b33ea8787d9c13dda
Alex Chan <alex@alexwlchan.net>
2024-04-17 00:35:31 +0100

    Cache the smartify filter

---
+++ src/_plugins/faster_smartify.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# This wraps Jekyll's built-in `smartify` plugin with a Jekyll Cache.
+#
+# This makes a significant impact on the speed of the site build,
+# cutting it roughly in half!
+
+module Jekyll
+  module Filters
+    alias_method :old_smartify, :smartify
+
+    def smartify(input)
+      cache = Jekyll::Cache.new('Smartify')
+
+      cache.getset(input) do
+        old_smartify(input)
+      end
+    end
+  end
+end
