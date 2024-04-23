9683bdf0d86590ab0475ab9a0610a4ac27e499e8
Alex Chan <alex@alexwlchan.net>
2024-04-23 06:43:34 +0100

    Move a couple of scripts inside this repo

---
+++ scripts/create_review.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'date'
+require 'pathname'
+require 'open3'
+
+print "What's the title of the book? "
+title = gets.chomp
+print "What's the slug of the MD file? "
+slug = gets.chomp
+print 'Who wrote the book? '
+author = gets.chomp
+print 'When was it published? '
+publication_year = gets.chomp
+
+today = Date.today
+year = today.year.to_s
+
+out_path = Pathname.new('src') + year + "#{slug}.md"
+
+out_path.dirname.mkpath # Create the directory if it doesn't exist
+
+File.write(out_path, <<~HEREDOC)
+  ---
+  layout: review
+  book:
+    contributors:
+      - name: #{author}
+    cover:
+      tint_color: "???"
+    title: "#{title}"
+    publication_year: #{publication_year}
+    tags:
+      - ???
+    isbn13: "???"
+  review:
+    date_read: ???
+    format: ???
+    rating: ???
+  ---
+HEREDOC
+
+# Use system call to open the file with the default application
+system('open', out_path.to_s)
